8e93fe25a07a415275b6730de29f48dfeaeb2ba9
lin <...>
2026-06-15 11:33:43 -0700

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
 public class Algorithms {
+    public static int linearSearch(ArrayList<Integer> arr, int target){
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            if (arr.get(i) == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(ArrayList<Integer>arr, int target ){
+        int n = arr.size();
+        System.out.println("Sorted Array: " + arr.toString());
+        int low =0;
+        int high = n-1;
+
+        while (low <=high){
+            int mid = low + (high-low) / 2;
+            if(arr.get(mid) == target){
+                return mid;
+            }
+            else if (arr.get(mid) < target){
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner scan =  new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        System.out.println("Enter your preferred size for the array: ");
+        int n = scan.nextInt();
+        for (int i = 0; i <n; i++){
+            System.out.printf("Enter element %d: %n", i+1);
+            arr.add(scan.nextInt());
+        }
+        System.out.println();
+
+
+        System.out.println(arr.toString());
+        System.out.println("Enter the target element you want to find: ");
+        int target = scan.nextInt();
+        System.out.println();
+
+        int linearSearchResult = Algorithms.linearSearch(arr, target);
+        System.out.println("The target element using Linear Search is found at index: " + linearSearchResult);
+        System.out.println();
+
+
+        ArrayList<Integer>sortedArr = new ArrayList<>(arr);
+        sortedArr.sort(null);
+        int binarySearchResult = Algorithms.binarySearch(sortedArr, target);
+        System.out.println("The target element using Binary Search is found at index: " + binarySearchResult);
     }
 }
+
