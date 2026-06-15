31f8eb83b74bde305c269de9781a2b548d1ef251
Your Name <you@example.com>
2026-06-15 17:19:13 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,37 @@
 public class Algorithms {
+      public static int linearSearch(int[] array, int target) {
+        // Your implementation here
+        
+        for (int i = 0; i < array.length; i++) {
+            if(target == array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+        public static int binarySearch(int[] array, int target) {
+        // Your implementation here
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(target == array[mid]){
+                return mid;
+            }
+
+            else if(array[mid] < target){    
+                low = mid + 1;
+            }
+            
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return -1; 
+    }
 }
