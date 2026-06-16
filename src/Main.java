be84735be5a704c4a5ef9c40368d1d01cda2d8dd
AnsuBadu <vbansu@st.ug.edu.gh>
2026-06-16 13:08:35 +0000

    Sorts user input array if it is not sorted

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the array:(The array must be sorted for binary search) ");
+        System.out.print("Enter the size of the array:");
         int n = scanner.nextInt();
         int[] arr = new int[n];
         
+        System.out.println("**The array should be sorted for binary search to work correctly.");
         for (int i = 0; i < n; i++) {
             System.out.print("Enter element " + (i + 1) + ": ");
             arr[i] = scanner.nextInt();
@@ -21,6 +23,8 @@ public class Main {
         } else {
             System.out.println("Element not found");
         }
+
+        Arrays.sort(arr); // Ensures the array is sorted if user input is not sorted
 
         int binarySearchResult = Algorithms.binarySearch(arr, target);
         System.out.println("Binary Search Result:");
