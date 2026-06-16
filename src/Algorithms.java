543fe21c56afd912dd7d562b140fa04f23ede4f6
Glory Osei Wusu Afriyie <gowafriyie@st.ug.edu.gh>
2026-06-16 20:24:46 +0000

    I implemented the binary and linear search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,31 @@
 public class Algorithms {
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; //if target is found
+            }
+        }
+        return -1; //if target is not found
+    }
+
+
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // if not found
+    }
 }
