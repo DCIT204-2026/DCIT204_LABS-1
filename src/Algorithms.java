647a81d6908ad74911e774e1667ddc537dced2cb
Ma-jesta <majestaobboheshun@gmail.com>
2026-06-16 13:14:37 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,28 @@
 public class Algorithms {
+    public static int linearSearch(int[] a, int key) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] a, int key) {
+        int start = 0;
+        int end = a.length - 1;
+
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if(key == a[mid]){
+                return mid;
+            }
+            else if(key < a[mid]){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+}
 }
