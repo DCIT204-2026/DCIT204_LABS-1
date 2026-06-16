005da45871f7f342067a6087ef952122fd9c1569
1mos-droid <isemuk8@gmail.com>
2026-06-16 21:41:57 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,48 @@
+/**
+ * Utility class containing common search algorithms.
+ */
 public class Algorithms {
+
+    /**
+     * Performs a linear search on an array.
+     *
+     * @param array  The array to search through.
+     * @param target The value to search for.
+     * @return The index of the target if found, otherwise -1.
+     */
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Performs a binary search on a sorted array.
+     *
+     * @param array  The sorted array to search through.
+     * @param target The value to search for.
+     * @return The index of the target if found, otherwise -1.
+     */
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
