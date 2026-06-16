7ffb5b447386e52fd5caa1ff22ea74c8f04904dd
Elizabeth Acheampomaa Darko <eadarko025@st.ug.edu.gh>
2026-06-16 15:38:18 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,34 @@
-public class Algorithms {
+public class Algorithm {
+
+    // Linear Search
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // found
+            }
+        }
+        return -1; // not found
+    }
+
+    // Binary Search (array must be sorted)
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 }
