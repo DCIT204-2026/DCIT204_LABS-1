6ea8cb1fc07c60d80467e80c773ee33e5eac038f
Elyon9 <eabamfo005@st.ug.edu.gh>
2026-06-16 23:40:50 +0000

    Implemented linear Search and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -25,9 +25,9 @@ public class Main {
         System.out.println("\n Using Linear Search");
         int linearResult = Algorithms.linearSearch(array, target);
         if (linearResult != -1) {
-            System.out.println("Target FOUND at index: " + linearResult);
+            System.out.println("Target Found at index: " + linearResult);
         } else {
-            System.out.println("Target NOT FOUND in the array.");
+            System.out.println("Target NOT Found in the array.");
         }
 
         int[] sortedArray = Arrays.copyOf(array, array.length);
@@ -37,9 +37,9 @@ public class Main {
         System.out.println("Sorted array used: " + Arrays.toString(sortedArray));
         int binaryResult = Algorithms.binarySearch(sortedArray, target);
         if (binaryResult != -1) {
-            System.out.println("Target FOUND at index: " + binaryResult + " (in sorted array)");
+            System.out.println("Target Found at index: " + binaryResult + " (in sorted array)");
         } else {
-            System.out.println("Target NOT FOUND in the array.");
+            System.out.println("Target NOT Found in the array.");
         }
 
         scanner.close();
