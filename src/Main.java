502112184931c17958ac6a286f610f022c01ca50
Abrahhhh <matwerefou@st.ug.edu.gh>
2026-06-16 19:42:11 +0000

    Added linear and binary search functionality
    Implemented linear and binary search algorithms with user input for array size and elements.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the target value to search for: ");
+        int target = scanner.nextInt();
+
+        int linearResult = Algorithm.linearSearch(arr, target);
+        System.out.println("\n--- Linear Search Result ---");
+        if (linearResult != -1) {
+            System.out.println("Target found at index: " + linearResult);
+        } else {
+            System.out.println("Target not found in the array.");
+        }
+        int[] sortedArr = new int[size];
+        for (int i = 0; i < size; i++) {
+            sortedArr[i] = arr[i]; 
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (sortedArr[j] > sortedArr[j + 1]) {
+                    
+                    int temp = sortedArr[j];
+                    sortedArr[j] = sortedArr[j + 1];
+                    sortedArr[j + 1] = temp;
+                }
+            }
+        }
+
+        int binaryResult = Algorithm.binarySearch(sortedArr, target);
+        System.out.println("\n--- Binary Search Result ---");
+        System.out.println("(Note: The array was sorted internally for this search)");
+        
+        if (binaryResult != -1) {
+            System.out.println("Target found at index: " + binaryResult);
+        } else {
+            System.out.println("Target not found in the array.");
+        }
+
+        scanner.close();
     }
 }
