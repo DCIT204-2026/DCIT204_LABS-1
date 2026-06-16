32112f6d895689621201cb61e36883b0d3fe688b
MaveSarpong <mssarpong003@ug.st.edu.gh>
2026-06-16 14:57:42 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,28 @@
 public class Algorithms {
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int l = 0;
+        int r = array.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 }
