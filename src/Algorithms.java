2c9e3126863a9dc47d497f0f5987b9f3e11760ff
Kofi Sakyi Agyei <kofisakyi430@gmail.com>
2026-06-17 08:08:52 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,40 @@
 public class Algorithms {
+
+    
+    public static int linearSearch(int[] arr, int num) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == num) {
+                return i;
+            }
+
+        }
+
+        return -1;
+    }
+
+   
+    public static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int middle = (low + high) / 2;
+
+            if (arr[middle] == target) {
+                return middle;
+            }
+
+            if (target < arr[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+
+        return -1;
+    }
 }
