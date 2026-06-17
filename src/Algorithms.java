a526e9c4bcf1789e018f8aca89dae3e487a6db0e
Evans Arthur <evansarthur622@gmail.com>
2026-06-17 04:58:57 +0000

    Implemented Linear and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,32 @@
-public class Algorithms {
+public class Algorithm {
+
+    // LINEAR SEARCH
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;  // Found! Return the index
+            }
+        }
+        return -1;  // Not found
+    }
+
+    // BINARY SEARCH
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;       // Found! Return the index
+            } else if (array[mid] < target) {
+                left = mid + 1;   // Search right half
+            } else {
+                right = mid - 1;  // Search left half
+            }
+        }
+        return -1;  // Not found
+    }
+
 }
