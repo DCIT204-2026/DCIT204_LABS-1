0e4abdbd0cef5146924da8a5f6de8559f89353f4
KuteChica <bridgettossou13@gmail.com>
2026-06-17 01:59:09 +0000

    Implemented Linear Search and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("\nEnter target value: ");
+        int target = input.nextInt();
+
+        int linearResult = Algorithms.linearSearch(arr, target);
+
+        System.out.println("\nLinear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Target found at index " + linearResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+
+        int binaryResult = Algorithms.binarySearch(sortedArr, target);
+
+        System.out.println("\nBinary Search Result:");
+        if (binaryResult != -1) {
+            System.out.println("Target found at index " + binaryResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        input.close();
     }
 }
