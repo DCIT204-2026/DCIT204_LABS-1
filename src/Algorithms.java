db64c59e61c4b979beefa4149c54fc6166d121a2
Mimi Appiah <mabappiah@st.ug.edu.gh>
2026-06-17 07:37:19 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,28 @@
-public class Algorithms {
+public static int linearSearch (int[] arr, int key) {
+    for (int i=0;i < arr.length; i++ ) {
+       if (arr[i]) == key ) {
+        return i;
+       }
+    }
+    else
+        return -1
+}
+
+
+
+public static binarySearch(int[] arr, int key) {
+    int start = 0
+    int end = arr.length - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key) {
+            return mid
+        } else if (arr[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+      int[] values = {5,10,17,27,30,38};
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number you want to look  for: ");
+        int key = input.nextInt();
 
+        int linearOutcome = Algorithms.linearSearch(values, key);
+        int binaryOutcome = Algorithms.binarySearch(values, key);
+
+        System.out.println("Linear Search outcome (index): " + linearOutcome);
+        System.out.println("Binary Search outcome (index): " + binaryOutcome);
+
+        input.close();
     }
 }
+
