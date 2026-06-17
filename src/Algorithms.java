470a4aebe197efd52f21e99386044ef99e2a3aaf
nyama-aa <anfrimpong-anin@st.ug.edu.gh>
2026-06-17 11:54:41 +0000

    Implelementation of linear search and binary search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,30 @@
-public class Algorithms {
+public class Algorithms { // the implementation for the linear search
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // return index if found
+            }
+        }
+        return -1;
+    }
+
+    // the implemmentation for the Binary Search
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (array[middle] == target) {
+                return middle; // found
+            } else if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
 }
