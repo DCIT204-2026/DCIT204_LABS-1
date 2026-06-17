680c24ba08e75f77e0b63250d8e298854a7422d6
Brew Araba <aabrew001@st.ug.edu.gh>
2026-06-17 12:54:05 +0000

    Implemented Linear Search and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr1 = {5, 3, 8, 1, 9};
+        int[] arr2 = {1, 3, 5, 7, 9};
+
+        System.out.println("Linear Search: " +
+                Algorithms.linearSearch(arr1, 8));
+
+        System.out.println("Binary Search: " +
+                Algorithms.binarySearch(arr2, 7));
     }
 }
