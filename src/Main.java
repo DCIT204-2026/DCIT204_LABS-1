6e5361988301e5325d959e6425759755606b2954
caankamah <caankamah@st.ug.edu.gh>
2026-06-17 01:02:11 +0000

    Implemented Linear Search and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter array size: ");
+
+        int n = scanner.nextInt();
+        int array[] = new int[n];
+        System.out.println("Make sure your array is sorted.");
+
+        for (int i = 1; i <= n; i++){
+            System.out.println("Enter element " + i + ":");
+            array[i - 1] = scanner.nextInt();
+        }
+        
+
+        System.out.println("Enter target value: ");
+
+        int target = scanner.nextInt();
+
+        System.out.println("Linear Search Result: " + "Target found at index " + Algorithms.linearSearch(array, target));
+        System.out.println("Binary Search Result: " + "Target found at index " + Algorithms.binarySearch(array, target));
+
+        scanner.close();
     }
 }
