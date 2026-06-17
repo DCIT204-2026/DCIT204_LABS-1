54edb76f9da8688f3419803d840942b4cb3ac5f1
poptonydm <poptonydm@gmail.com>
2026-06-17 08:03:44 -0800

    first commit

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,36 @@
 public class Algorithms {
+
+    public static int linearSearch(int n, int[] array, int target) {
+        for (int i = 0; i < n; i++) {
+            if(array[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int binarySearch(int n, int[] array, int target) {
+        int start = 0;
+        int end = n-1;
+
+        if(array[start] == target){
+            return start;
+        }else if(array[end] == target){
+            return end;
+        }
+
+        while(start != end){
+            int mid = (int) Math.floor((end - start) / 2);
+
+            if(array[mid] > target){
+                end = mid - 1;
+            }else if(array[mid] < target){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+    public static int input(String prompt) {
+        System.out.println(prompt);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        return n;
+    }
+
+    public static int[] input(String prompt, int n) {
+        System.out.println(prompt);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                System.out.print("Enter Element " + (i + 1) + ": ");
+                while (!scanner.hasNextInt()) {
+                    System.out.print("Invalid. Enter an integer: ");
+                    scanner.next();
+                }
+                arr[i] = scanner.nextInt();
+            }
+
+            Arrays.sort(arr);
+            return arr;
+        }
+        
+    }
+    
     public static void main(String[] args) {
 
+        int n = input("Enter Array Size");
+        int target = input("Enter Target");
+        int[] array = input("Enter Elements...", n);
+        
+        int linearSearchResult = Algorithms.linearSearch(n, array, target);
+        int binarySearchResult = Algorithms.binarySearch(n, array, target);
+
+        System.out.println(linearSearchResult);
+        System.out.println(binarySearchResult);
+        
     }
+
 }
