059974b8f7db96942be6c6b6c43634e3c743aa69
Ish <nlilartey@st.ug.edu.gh>
2026-06-17 00:05:38 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,33 @@
 public class Algorithms {
+
+    public static int linearSearch(int[] array, int target) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] array, int target) {
+
+        int min = 0;
+        int max = array.length - 1;
+
+        while (min <= max) {
+            int middle = (min + max) / 2;
+            if (array[middle] == target) {
+                return middle;
+            }
+            if (array[middle] < target) {
+                min = middle + 1;
+            } 
+            else {
+                max = middle - 1;
+            }
+        }
+        return -1;
+    }
+
 }
