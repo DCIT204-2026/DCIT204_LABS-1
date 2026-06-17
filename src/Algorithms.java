a57ab216ad1e6cc30c8555d78a38d2752800c97c
Samuellopez77 <slasumani@st.ug.edu.gh>
2026-06-17 01:24:12 +0000

    Implemented Linear and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,32 @@
 public class Algorithms {
+    //linear search
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    // binary search
+    public static int binSearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+       //linear search
+
+        Scanner input = new Scanner(System.in);
+
+        // read array arraySize and initialize array
+
+        System.out.print("Enter array arraySize: ");
+        int arraySize = input.nextInt();
+
+        int[] arr = new int[arraySize];
+
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < arraySize; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.print("Enter Target value: ");
+        int target = input.nextInt();
+
+        int result = Algorithms.linearSearch(arr, target);
+
+        System.out.println("\n LINEAR SEARCH RESULT");
+
+        if (result != -1) {
+            System.out.println("Target found at index: " + result);
+        } else {
+
+            System.out.println("Target NOT FOUND");
+        }
+        //binary search
+
+        // Sort array before binary search
+        Arrays.sort(arr);
+
+        /*
+         divides the array into halves to search faster.
+         */
+
+        int binResult = Algorithms.binSearch(arr, target);
+
+        System.out.println("\nBINARY SEARCH RESULT");
+
+        if (binResult != -1) {
+            System.out.println("Target found at index: " + binResult);
+        } else {
+            System.out.println("Target NOT FOUND");
+        }
+        input.close();
     }
+
 }
