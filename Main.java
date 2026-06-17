9928956a0be605d6322e3ec9012576d55e83c43d
Epaphras-Asiedu <ekasiedu017@st.ug.edu.gh>
2026-06-17 09:53:43 +0000

    Implemented Linear and Binary Search

--- Main.java
+++ Main.java
@@ -4,47 +4,47 @@ import java.util.Scanner;
 
 public class Main { 
     public static void main(String[] args) {
-        Scanner consoleInput = newScanner(System.in);
-
-        System.out.print("Eneter array size please");
-        int totalElements = consoleInput.nextInt();
-
-        int[] dataValues = new int[totalElements];
-
-        System.out.println();
-        for (int i = 0; i < dataValues.length; i++) {
-            System.out.print("Enter your elements " + (i+1) + ": ");
-            dataValues[i] = consoleInput.nextInt();
-        }   
-
-        System.out.println();
-        System.out.print("Enter your target value please");
-        int searchTarget = consoleInput.nextInt();
-        System.out.println();
-
-        //Execution of linear search
-        int linearIndex = Algorithm.linearSearch(dataValues,searchTarget);
-        System.out.println("Linear Search result");
-        if (linearIndex != -1) {
-            System.out.println("Target found at index " + linearIndex);
-        } else {
-            System.out.println("Target not found");
+        try (Scanner consoleInput = newScanner(System.in)) {
+            System.out.print("Eneter array size please");
+            int totalElements = consoleInput.nextInt();
+            
+            int[] dataValues = new int[totalElements];
+            
+            System.out.println();
+            for (int i = 0; i < dataValues.length; i++) {
+                System.out.print("Enter your elements " + (i+1) + ": ");
+                dataValues[i] = consoleInput.nextInt();
+            }
+            
+            System.out.println();
+            System.out.print("Enter your target value please");
+            int searchTarget = consoleInput.nextInt();
+            System.out.println();
+            
+            //Execution of linear search
+            int linearIndex = Algorithm.linearSearch(dataValues,searchTarget);
+            System.out.println("Linear Search result");
+            if (linearIndex != -1) {
+                System.out.println("Target found at index " + linearIndex);
+            } else {
+                System.out.println("Target not found");
+            }
+            System.out.println();
+            Arrays.sort(dataValues);
+            int binaryIndex = Algorithm.binarySearch(dataValues, searchTarget);
+            System.out.println("Binary Search result");
+            if (binaryIndex != -1) {
+                System.out.println("Target found at index " + binaryIndex);
+            } else {
+                System.out.println("Target not found");
+            }
         }
-        System.out.println();
-        Arrays.sort(dataValues);
-        int binaryIndex = Algorithm.binarySearch(dataValues, searchTarget);
-        System.out.println("Binary Search result");
-        if (binaryIndex != -1) {
-            System.out.println("Target found at index " + binaryIndex);
-        } else {
-            System.out.println("Target not found");
-        }
-        consoleInput.close();
     }
 
-    private static Scanner newScanner(InputStream in) {
+    private static Scanner newScanner(@SuppressWarnings("unused") InputStream in) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
 
 
 
