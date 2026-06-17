45a444832fd9173298caca3cf42594fa89c02c57
KAY461 <kdowusu-agyemang@st.ug.edu.gh>
2026-06-17 06:59:23 +0000

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,28 @@
 public class Algorithms {
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // found, return index
+            }
+        }
+        return -1; // not found
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (array[mid] == target) {
+                return mid; // found
+            } else if (array[mid] < target) {
+                low = mid + 1; // search right half
+            } else {
+                high = mid - 1; // search left half
+            }
+        }
+        return -1; // not found
+    }
 }
