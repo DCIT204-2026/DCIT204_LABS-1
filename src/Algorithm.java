8430a5847f39ce04765b7c58578ca46cf939615a
faculty-NA <haidoo011@st.ug.edu.gh>
2026-06-17 01:36:10 +0000

    Final submission - Student ID 22380780

---
+++ src/Algorithm.java
@@ -0,0 +1,32 @@
+public class Algorithm {
+
+    // Linear Search (char array)
+    public static int linearSearch(char[] array, char target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Binary Search (char array)
+    public static int binarySearch(char[] array, char target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter number of letters: ");
+        int size = input.nextInt();
+
+        char[] array = new char[size];
+
+        // Input letters
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter letter " + (i + 1) + ": ");
+            array[i] = input.next().charAt(0);
+        }
+
+        // Input target
+        System.out.print("Enter target letter: ");
+        char target = input.next().charAt(0);
+
+        // Linear Search
+        int linearResult = Algorithm.linearSearch(array, target);
+
+        System.out.println("\nLinear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Letter found at index " + linearResult);
+        } else {
+            System.out.println("Letter not found");
+        }
+
+        // Sort for Binary Search
+        Arrays.sort(array);
+
+        // Binary Search
+        int binaryResult = Algorithm.binarySearch(array, target);
+
+        System.out.println("\nBinary Search Result (Sorted Array):");
+        System.out.println(Arrays.toString(array));
+
+        if (binaryResult != -1) {
+            System.out.println("Letter found at index " + binaryResult);
+        } else {
+            System.out.println("Letter not found");
+        }
+
+        input.close();
     }
 }
