32187cd232032eae5bef4cdaf7715843ba789e3c
SoftlyTina <addoaugustina20@gmail.com>
2026-06-17 02:27:04 +0000

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,31 @@
 public class Algorithms {
+
+    // Linear Search Algorithm
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found, return its index
+            }
+        }
+        return -1; // Target not found
+    }
+
+    // Binary Search Algorithm
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found, return its index
+            } else if (arr[mid] < target) {
+                low = mid + 1; // Search in the right half
+            } else {
+                high = mid - 1; // Search in the left half
+            }
+        }
+        return -1; // Target not found
+    }
 }
